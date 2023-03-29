f24ddf1b5b7fc8f2a5f41ffcc6303fb16864b9cc
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-03-29 15:11:35 +0300

    refactor: delete webVitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./containers/App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
@@ -14,8 +13,3 @@ root.render(
     </Router>
   </AuthProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
