a60f69c49274f4a81adcb6111ab498498472ded2
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-04-09 20:13:35 +0300

    feat: add home routing on logo click

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,10 +2,13 @@ import React from "react";
 import Tilt from "react-tilt";
 
 import "./Logo.css";
+import { useNavigate } from "react-router-dom";
 
 const Logo = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="logo">
+    <div className="logo" onClick={() => navigate("/")}>
       <Tilt className="Tilt" options={{ max: 35 }}>
         <div className="Tilt-inner">
           <img alt="logo" src="favicon.ico" />
