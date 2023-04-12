161114b8b6923b498736eee2665bd922862444c3
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-04-12 16:11:14 +0300

    feat: update profile css

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,20 +3,26 @@ import useAuth from "../../hooks/useAuth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faIdCard } from "@fortawesome/free-solid-svg-icons";
 import "./Profile.css";
+import { userLocale } from "../../util/locale";
 
 const Profile = () => {
   const { auth } = useAuth();
 
+  const creationDate = new Date(auth.createdAt).toLocaleDateString(userLocale, {
+    hour: "numeric",
+    minute: "numeric",
+  });
+
   return (
     <div className="profile-section">
-      <h2>
+      <h2 className="profile-section__title">
         <FontAwesomeIcon icon={faIdCard} /> User profile
       </h2>
-      {`Login: ${auth.login}`}
-      <br />
-      {`Creation date: ${new Date(auth.createdAt)}`}
-      <br />
-      {`Role: ${auth.role}`}
+      <div className="profile-section__content">
+        <p>Login: {auth.login}</p>
+        <p>Creation date: {creationDate}</p>
+        <p>Role: {auth.role}</p>
+      </div>
     </div>
   );
 };
