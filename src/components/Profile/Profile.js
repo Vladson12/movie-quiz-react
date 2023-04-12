93c93175e7994ed312a5bf82a903eef4cdbe5f3a
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-04-12 15:48:49 +0300

    feat: update profile css

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faIdCard } from "@fortawesome/free-solid-svg-icons";
+import "./Profile.css";
 
 const Profile = () => {
   const { auth } = useAuth();
 
   return (
-    <div>
-      <FontAwesomeIcon icon={faIdCard} />
-      <br />
+    <div className="profile-section">
+      <h2>
+        <FontAwesomeIcon icon={faIdCard} /> User profile
+      </h2>
       {`Login: ${auth.login}`}
       <br />
       {`Creation date: ${new Date(auth.createdAt)}`}
